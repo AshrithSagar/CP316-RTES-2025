1b83c306732e531b5c75bfdfb080bcf1bd1287fe
Ashrith Sagar <ashrithy@iisc.ac.in>
2025-02-05 09:11:24 +0530

    A5

--- Assignments/Assignment-5/Codes/main.c
+++ Assignments/Assignment-5/Codes/main.c
@@ -1,19 +1,15 @@
 #include "os.h"
 
-void task1(void)
-{
-    while (1)
-    {
-        task_yield();
-    }
+void task1(void) {
+  while (1) {
+    task_yield();
+  }
 }
 
-int main(void)
-{
-    task_create(task1);
+int main(void) {
+  task_create(task1);
 
-    while (1)
-    {
-        task_yield();
-    }
+  while (1) {
+    task_yield();
+  }
 }
