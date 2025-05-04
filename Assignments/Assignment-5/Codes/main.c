01b129622def28fadd164f38d65bb87794b4950a
Ashrith Sagar <ashrithy@iisc.ac.in>
2025-05-04 12:29:41 +0530

    A5

--- Assignments/Assignment-5/Codes/main.c
+++ Assignments/Assignment-5/Codes/main.c
@@ -1,30 +1,25 @@
-#include "bsp/myled.h"
+#include "bsp/bsp.h"
 #include "os.h"
 
 #define FPCCR (*(volatile long*)0xE000EF34)
 
 void task1(void) {
-    led_on(2, 2);
-    delay(1000);
+    int count = 0;
     while (1) {
+        printf("Task 1: %d\n", count++);
         task_yield();
-        led_on(4, 4);
-        delay(1000);
     }
 }
 
 int main(void) {
-    led_on(0, 0);
-    delay(1000);
-    FPCCR &= ~(1 << 30);
+    int count = 0;
 
+    FPCCR &= ~(1 << 30);
+    bsp_init();
     task_create(task1);
-    led_on(1, 1);
-    delay(1000);
 
     while (1) {
+        printf("Main: %d\n", count++);
         task_yield();
-        led_on(3, 3);
-        delay(1000);
     }
 }
