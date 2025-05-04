429667ffdb4f2855f92bf253e374ec377464c934
Ashrith Sagar <ashrithy@iisc.ac.in>
2025-05-04 08:58:45 +0530

    A7

---
+++ Assignments/Assignment-7/Codes/bsp/bsp.h
@@ -0,0 +1,15 @@
+#ifndef BSP_H
+#define BSP_H
+
+#include "adc.h"
+#include "audio.h"
+#include "gpio.h"
+#include "ledbtn.h"
+#include "microbit.h"
+#include "printf.h"
+#include "pwm.h"
+#include "uart.h"
+
+void bsp_init(void);
+
+#endif /* BSP_H */
