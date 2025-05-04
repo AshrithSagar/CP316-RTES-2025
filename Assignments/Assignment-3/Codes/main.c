6c28393b33452edc7a0acc8d0c59b8ef38f060bb
Ashrith Sagar <ashrithy@iisc.ac.in>
2025-05-04 15:58:13 +0530

    A3

--- Assignments/Assignment-3/Codes/main.c
+++ Assignments/Assignment-3/Codes/main.c
@@ -4,10 +4,11 @@ int main() {
     int audio_freq = 1000;
     int ball_r = 0, ball_c = 0;
     int catch_r = 4, catch_c = 2;
+    int ball_speed = 200, score = 0;
 
     bsp_init();
 
-    uart_puts("[START] Game of Catch!\n");
+    printf("[START] Game of Catch!\n[INFO] Score: %d\n", score);
     audio_sweep(500, 2000, 100);
     timer_start(0, 5, led_row_refresh);
 
@@ -20,26 +21,44 @@ int main() {
         if (button_get(0)) {
             catch_c = (catch_c - 1 + LED_NUM_COLS) % LED_NUM_COLS;  // Move left
             frame_buffer[catch_r][catch_c] = 1;
+            printf("[INFO] Catch moved left\n");
             audio_sweep(500, 2000, 50);
         }
         if (button_get(1)) {
             catch_c = (catch_c + 1) % LED_NUM_COLS;  // Move right
             frame_buffer[catch_r][catch_c] = 1;
+            printf("[INFO] Catch moved right\n");
             audio_sweep(2000, 500, 50);
         }
         frame_buffer[catch_r][catch_c] = 1;
         timer_delay(50);
 
         frame_buffer[ball_r][ball_c] = 1;
-        timer_delay(200);
+        timer_delay(ball_speed);
+#if 0  // Can get annoying during debugging
+        audio_beep(audio_freq, 20);
+#endif
         frame_buffer[ball_r][ball_c] = 0;
 
         ball_r++;
         audio_freq *= 1.1;
         if (ball_r == LED_NUM_ROWS) {
+            // Ball reached the bottom
             audio_freq = 1000;
+            // Reset ball position
             ball_r = 0;
             ball_c = (ball_c + 1) % LED_NUM_COLS;
+        } else if (ball_r == catch_r && ball_c == catch_c) {
+            // Collision detection
+            audio_sweep(500, 2000, 50);
+            if (ball_speed < 50)
+                ball_speed = 50;  // Minimum speed
+            else
+                ball_speed -= 10;  // Increase speed
+            // Reset ball position
+            ball_r = 0;
+            ball_c = (ball_c + 1) % LED_NUM_COLS;
+            printf("[INFO] Catch successful! Score: %d\n", ++score);
         }
     }
 
